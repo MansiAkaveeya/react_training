07d86d7a2bc4288956039acd5d0fbdf8310ff907
mansi.akaveeya <mansi.akaveeya@theonetechnologies.co.in>
2020-06-17 11:43:38 +0530

    Updates

---
+++ precache-manifest.225fc319f04cbb1a1d423e1d9438fbbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190bbfa7c9fe0e8833a6b5d0e4f98c30",
+    "url": "/MansiAkaveeya/react_training/index.html"
+  },
+  {
+    "revision": "95c0feb990af1241bd31",
+    "url": "/MansiAkaveeya/react_training/static/css/main.cfb5a67d.chunk.css"
+  },
+  {
+    "revision": "36f037ca2a3c7fd8e117",
+    "url": "/MansiAkaveeya/react_training/static/js/2.b072d574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MansiAkaveeya/react_training/static/js/2.b072d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c0feb990af1241bd31",
+    "url": "/MansiAkaveeya/react_training/static/js/main.0da62e7a.chunk.js"
+  },
+  {
+    "revision": "a542943a0826c9de6133",
+    "url": "/MansiAkaveeya/react_training/static/js/runtime-main.6093df29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MansiAkaveeya/react_training/static/media/logo.5d5d9eef.svg"
+  }
+]);
